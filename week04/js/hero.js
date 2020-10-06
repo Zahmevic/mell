ccb886f018e358a174efee256a55c7c35ea43977
SacNicte-C <mmczaho@gmail.com>
2020-10-06 17:28:29 -0500

    update

--- week04/js/hero.js
+++ week04/js/hero.js
@@ -20,3 +20,27 @@ if (form.powers[i].checked) {
 hero.powers.push(form.powers[i].value);
 }
 }
+
+form.addEventListener('submit',validate,false);
+function validate(event) {
+const firstLetter = form.heroName.value[0];
+if (firstLetter.toUpperCase() === 'X') {
+event.preventDefault();
+alert('Your name is not allowed to start with X!');
+}
+}
+
+const label = form.querySelector('label');
+const error = document.createElement('div');
+error.classList.add('error');
+error.textContent = '! Your name is not allowed to start with X.';
+label.append(error);
+function validateInline() {
+const heroName = this.value.toUpperCase();
+if(heroName.startsWith('X')){
+error.style.display = 'block';
+} else {
+error.style.display = 'none';
+}
+}
+
