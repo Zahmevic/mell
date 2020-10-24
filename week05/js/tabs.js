5102c4515eb2ef002db982f4619d8630e2821bda
gianut <mzahorimc@hotmail.com>
2020-10-23 21:59:09 -0500

    update

---
+++ week05/js/tabs.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function active(){
+    $('ul.tabs li a:first').addClass('active');
+    $('.sections #all').hide();
+    $('sections #all:first').show();
+});
--- week05/js/todo.js
+++ week05/js/todo.js
@@ -4,42 +4,20 @@ const button = document.querySelector("button");
 
 function addNote() {
     const file = document.createElement("tr");
-
     file.innerHTML = `
     <td><input type="checkbox" onClick="complete()" /></td>
     <td style="flex-grow: 2"> ${input.value} </td>
     <td> <span onClick="borrar()"> X </span> </td>
     `;
-
     table.appendChild(file);
-
 }
 
 button.addEventListener('click', function(e){
-    if(input.value === ""){
-        validation("Introduce a note", "fail");
-    } else {
         addNote();
-        validation("Note added successfully", "success");
-    }
 });
 
 function borrar(event){
     this.event.target.parentElement.parentElement.remove();
-}
-
-function validation(message, classs){
-    const div = document.createElement('div');
-    div.className= classs;
-    div.appendChild(document.createTextNode(message));
-
-    const title = document.querySelector('h2');
-
-    title.insertBefore(div, null);
-
-    setTimeout(function(){
-        document.querySelector(`.${classs}`).remove();
-    }, 3000)
 }
 
 function complete(event){
@@ -49,3 +27,4 @@ function complete(event){
         this.event.target.parentElement.parentElement.classList.remove("complete");
     }
     }
+
