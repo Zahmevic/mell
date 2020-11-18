089c6205e7aab75b787ca57a83a8cde035a24d3a
gianut <mzahorimc@hotmail.com>
2020-11-17 23:36:19 -0600

    update

--- week10/utilities.js
+++ week10/utilities.js
@@ -1,6 +1,6 @@
 //const requestURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-  //  fetch(requestURL)
+  //  fetch('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02')
   //.then(function (response) {
     //return response.json();
   //})
@@ -12,7 +12,7 @@
 //});
 
 export function getJSON(url) {
-    return fetch(url)
+fetch(url)
         .then(function(response) {
             if (!response.ok) {
                 throw Error(response.statusText);
